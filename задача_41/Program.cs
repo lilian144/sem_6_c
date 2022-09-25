791c15cd3f3555acb30c0ce7cf14a21599334fca
Alena <qlushenko36@gmail.com>
2022-09-25 23:22:59 +0300

    задача 41

---
+++ задача_41/Program.cs
@@ -0,0 +1,31 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь. 
+// Ввод чисел останавливается при помощи ввода стоп-слова "stop"
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Введите числа. При помощи ввода стоп-слова stop остановите ввод чисел");
+int ReadNumber(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int count = 0;
+
+while(true)
+{
+    string answer = Console.ReadLine();
+    if(answer == "stop")
+    {
+        break;
+    }
+    else{
+        int answerNumber = Convert.ToInt32(answer);
+        if(answerNumber > 0)
+        {
+            count++;
+        }
+    }
+}
+
+Console.WriteLine(count);
